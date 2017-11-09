84d90ce9fb44f72f6ba36a2e2347351776bc65cf
agennx <ahsan@brilliantinnovators.com>
2017-11-09 16:17:26 +0500

    3rd message

--- footer.php
+++ footer.php
@@ -19,7 +19,12 @@
         </script>
 
         <!-- jQuery  -->
-
+        <script src="js/plate101.js"></script>
+		<script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/waves.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
         <script src="js/jquery.scrollTo.min.js"></script>
         <script src="assets/chat/moment-2.2.1.js"></script>
         <script src="assets/jquery-sparkline/jquery.sparkline.min.js"></script>
